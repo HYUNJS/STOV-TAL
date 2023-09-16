c1928e63b4448ec36870bb197eb0d0da71fb12f7
HYUNJS <jhyunaa@connect.ust.hk>
2023-09-16 18:17:58 +0000

    config update

---
+++ train_TH14_PL_split50.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+th=$1
+# th=0.00
+# th=0.01
+# th=0.05
+VER=0
+D1=0
+D2=1
+
+echo threshold $th
+sleep 5;
+
+i=0
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-50-${i}_E-non50-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_50_test_50/validation_non50-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=$D1 python train.py configs/thumos14_split/thumos14_vifi_non50_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=1
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-50-${i}_E-non50-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_50_test_50/validation_non50-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=$D1 python train.py configs/thumos14_split/thumos14_vifi_non50_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=2
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-50-${i}_E-non50-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_50_test_50/validation_non50-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=$D1 python train.py configs/thumos14_split/thumos14_vifi_non50_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=3
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-50-${i}_E-non50-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_50_test_50/validation_non50-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=$D1 python train.py configs/thumos14_split/thumos14_vifi_non50_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=4
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-50-${i}_E-non50-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_50_test_50/validation_non50-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=$D1 python train.py configs/thumos14_split/thumos14_vifi_non50_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=5
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-50-${i}_E-non50-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_50_test_50/validation_non50-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=$D2 python train.py configs/thumos14_split/thumos14_vifi_non50_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=6
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-50-${i}_E-non50-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_50_test_50/validation_non50-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=$D2 python train.py configs/thumos14_split/thumos14_vifi_non50_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=7
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-50-${i}_E-non50-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_50_test_50/validation_non50-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=$D2 python train.py configs/thumos14_split/thumos14_vifi_non50_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=8
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-50-${i}_E-non50-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_50_test_50/validation_non50-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=$D2 python train.py configs/thumos14_split/thumos14_vifi_non50_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=9
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-50-${i}_E-non50-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_50_test_50/validation_non50-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=$D2 python train.py configs/thumos14_split/thumos14_vifi_non50_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json};
+sleep 60;
+
+
+# for ((i=0;i<=9;i++))
+# do
+#     train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-50-${i}_E-non50-$i_th-${th}_CLS-ep10.json"
+#     val_json="dataset.val_json_file ./data/thumos14/annotations/train_50_test_50/validation_50-${i}_tal.json"
+#     python train.py configs/thumos14_split/thumos14_vifi_non50_pseudo_tmpl.yaml --output 1 ${train_json} ${val_json}
+# done
---
+++ train_TH14_PL_split75.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+th=$1
+# th=0.00
+# th=0.01
+# th=0.05
+VER=0
+
+echo threshold $th
+sleep 3;
+
+i=0
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-75-${i}_E-non75-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_75_test_25/validation_non75-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=0 python train.py configs/thumos14_split/thumos14_vifi_non75_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=1
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-75-${i}_E-non75-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_75_test_25/validation_non75-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=0 python train.py configs/thumos14_split/thumos14_vifi_non75_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=2
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-75-${i}_E-non75-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_75_test_25/validation_non75-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=0 python train.py configs/thumos14_split/thumos14_vifi_non75_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=3
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-75-${i}_E-non75-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_75_test_25/validation_non75-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=0 python train.py configs/thumos14_split/thumos14_vifi_non75_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=4
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-75-${i}_E-non75-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_75_test_25/validation_non75-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=0 python train.py configs/thumos14_split/thumos14_vifi_non75_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=5
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-75-${i}_E-non75-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_75_test_25/validation_non75-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=1 python train.py configs/thumos14_split/thumos14_vifi_non75_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=6
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-75-${i}_E-non75-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_75_test_25/validation_non75-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=1 python train.py configs/thumos14_split/thumos14_vifi_non75_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=7
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-75-${i}_E-non75-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_75_test_25/validation_non75-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=1 python train.py configs/thumos14_split/thumos14_vifi_non75_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=8
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-75-${i}_E-non75-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_75_test_25/validation_non75-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=1 python train.py configs/thumos14_split/thumos14_vifi_non75_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json}&
+sleep 5;
+
+i=9
+train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-75-${i}_E-non75-${i}_th-${th}_CLS-ep10.json"
+val_json="dataset.val_json_file ./data/thumos14/annotations/train_75_test_25/validation_non75-${i}_tal.json"
+CUDA_VISIBLE_DEVICES=1 python train.py configs/thumos14_split/thumos14_vifi_non75_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${val_json};
+sleep 60;
+
+# for ((i=0;i<=9;i++))
+# do
+#     train_json="dataset.train_json_file ./data/thumos14/pseudo_annos_nonsplit/T-50-${i}_E-non50-$i_th-${th}_CLS-ep10.json"
+#     val_json="dataset.val_json_file ./data/thumos14/annotations/train_50_test_50/validation_50-${i}_tal.json"
+#     python train.py configs/thumos14_split/thumos14_vifi_non50_pseudo_tmpl.yaml --output 1 ${train_json} ${val_json}
+# done

ba04f7fb76be45b959b144a090933e14d0a96ff6
HYUNJS <jhyunaa@connect.ust.hk>
2023-10-09 20:47:45 +0000

    AN PL training

--- train_anet_prop_PL_split50.sh
+++ train_anet_prop_PL_split50.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-th=$1 # 0.00 | 0.01 | 0.05
-VER=0
+VER=3
 
 dataset='anet13'
-echo threshold $th $dataset
+# model='clip'
+# model='vifi'
+model=$1
+th=$2 # 0.00 | 0.01 | 0.05
+split="50"
+
+echo $dataset $th $model $split
 sleep 5;
 
+ckpt_freq="-c 2"
 # devices=(0 1 2 3 0 1 2 3 0 1)
-devices=(0 0 0 0 0 0 0 0 0 0)
-# devices=(2 2 2 2 2 2 2 2 2 2)
+devices=(0 1 0 1 0 1 0 1 0 1)
+# devices=(0 0 0 0 0 1 1 1 1 1)
 # devices=(2 2 2 2 2 3 3 3 3 3)
+# devices=(1 1 1 1 1 1 1 1 1 1)
 for ((i=0;i<=9;i++))
 do
-    train_json="dataset.train_json_file ./data/${dataset}/pseudo_annos_non50_all/T-50-${i}_E-non50-${i}_th-${th}_CLS-ep10.json"
+    train_json="dataset.train_json_file ./data/${dataset}/PL_non${split}_${model}/T-${split}-${i}_E-non${split}-${i}_th-${th}.json"
     split_id="dataset.split_id ${i}"
-    CUDA_VISIBLE_DEVICES=${devices[i]} python train.py configs/${dataset}_split/${dataset}_vifi_prop_non50_pseudo_tmpl.yaml --output split${i}_th${th}_${VER} --opts ${train_json} ${split_id}
-    sleep 3;
+    if [ $i -eq 1 ] || [ $i -eq 3 ] || [ $i -eq 5 ] || [ $i -eq 7 ] || [ $i -eq 9 ]
+    then
+        CUDA_VISIBLE_DEVICES=${devices[i]} python train.py configs/${dataset}_split/${dataset}_${model}_prop_non${split}_PL_tmpl.yaml ${ckpt_freq} --output split${i}_th${th}_${VER} --opts ${train_json} ${split_id}
+    else
+        CUDA_VISIBLE_DEVICES=${devices[i]} python train.py configs/${dataset}_split/${dataset}_${model}_prop_non${split}_PL_tmpl.yaml ${ckpt_freq} --output split${i}_th${th}_${VER} --opts ${train_json} ${split_id}&
+    fi
+    sleep 1;
 done
